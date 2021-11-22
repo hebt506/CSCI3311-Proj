ae3adc3b17d3b3e252bbc5553c6279ff24fd1155
Jinghong <hebt@bc.edu>
2021-11-22 16:41:15 -0500

    IP

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 d3.csv("howboutdis.csv", d3.autoType).then(data => {
 
-    // console.log("loading")
+    console.log("loading")
 
     const year = document.getElementById('result-year').innerHTML;
     var count = 0;
